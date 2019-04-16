6a2e563c41e03b1cd0e15044193dbcd149b8126e
ykiu <32252655+ykiu@users.noreply.github.com>
2019-04-16 21:27:35 +0900

    Refactor an existing test

--- normalizer/tests.py
+++ normalizer/tests.py
@@ -11,6 +11,65 @@ from normalizer import (
 
 sample_path = Path(__file__).parent.joinpath('sample.csv')
 
+SAMPLE_TAXA = (
+    Taxon(
+        'plants',
+        'cabombaceae',
+        0,
+        ['ジュンサイ科'],
+        ['Cabombaceae']
+    ),
+    Taxon(
+        'cabombaceae',
+        'brasenia',
+        0,
+        [],
+        ['Brasenia']
+    ),
+    Taxon(
+        'brasenia',
+        'brasenia_schreberi',
+        0,
+        ['ジュンサイ'],
+        ['Brasenia schreberi J.F.Gmel.']
+    ),
+    Taxon(
+        'plants',
+        'nymphaeaceae',
+        1,
+        ['スイレン科'],
+        ['Nymphaeaceae']
+    ),
+    Taxon(
+        'nymphaeaceae',
+        'euryale',
+        0,
+        [],
+        ['Euryale']
+    ),
+    Taxon(
+        'euryale',
+        'euryale_ferox',
+        0,
+        ['オニバス'],
+        ['Euryale ferox Salisb.']
+    ),
+    Taxon(
+        'nymphaeaceae',
+        'nuphar',
+        1,
+        [],
+        ['Nuphar']
+    ),
+    Taxon(
+        'nuphar',
+        'nuphar_japonica',
+        0,
+        ['コウホネ'],
+        ['Nuphar japonica DC.']
+    ),
+)
+
 
 class RowsToTaxaTest(TestCase):
 
@@ -22,65 +81,8 @@ class RowsToTaxaTest(TestCase):
         self._file.close()
 
     def test_normal(self):
-        expected = [
-            Taxon(
-                'plants',
-                'cabombaceae',
-                0,
-                ['ジュンサイ科'],
-                ['Cabombaceae']
-            ),
-            Taxon(
-                'cabombaceae',
-                'brasenia',
-                0,
-                [],
-                ['Brasenia']
-            ),
-            Taxon(
-                'brasenia',
-                'brasenia_schreberi',
-                0,
-                ['ジュンサイ'],
-                ['Brasenia schreberi J.F.Gmel.']
-            ),
-            Taxon(
-                'plants',
-                'nymphaeaceae',
-                1,
-                ['スイレン科'],
-                ['Nymphaeaceae']
-            ),
-            Taxon(
-                'nymphaeaceae',
-                'euryale',
-                0,
-                [],
-                ['Euryale']
-            ),
-            Taxon(
-                'euryale',
-                'euryale_ferox',
-                0,
-                ['オニバス'],
-                ['Euryale ferox Salisb.']
-            ),
-            Taxon(
-                'nymphaeaceae',
-                'nuphar',
-                1,
-                [],
-                ['Nuphar']
-            ),
-            Taxon(
-                'nuphar',
-                'nuphar_japonica',
-                0,
-                ['コウホネ'],
-                ['Nuphar japonica DC.']
-            ),
-        ]
-        actual = [*rows_to_taxa(
+        expected = SAMPLE_TAXA
+        actual = (*rows_to_taxa(
             rows=self.reader,
             sp_common_name_colname='新リスト和名',
             sp_common_name_syn_colname='和名異名',
@@ -89,7 +91,7 @@ class RowsToTaxaTest(TestCase):
             fa_scientific_name_colname='APG科名',
             root_key='plants',
             synonym_delimiter='，',
-        )]
+        ),)
         assert expected == actual, (
             f'\nexpected:\n{expected}\nactual:\n{actual}'
         )
